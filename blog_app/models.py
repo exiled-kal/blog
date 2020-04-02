b3b5199b3d9dc74189490a4dd70104c5395a4743
kaldne <tseringkalden1964@gmail.com>
2020-04-02 16:50:00 -0700

    added changes

--- blog_app/models.py
+++ blog_app/models.py
@@ -4,14 +4,26 @@ from django.db import models
 class User(models.Model):
     username = models.TextField()
     
+    
+    
+class BlogManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        
+        if len(postData['description'])< 5:
+            errors["description"] = "Blog name should be at least 5 characters"
+            return errors
+    
 class Blog(models.Model):
     description = models.TextField()
     user = models.ForeignKey(User,on_delete=models.DO_NOTHING,related_name="blogs",blank=True, null=True)
     users_who_liked = models.ManyToManyField(User,related_name="blogs_liked")
+    objects = BlogManager()
     
     
     def __str__(self):
         return f"Blog id {self.id} description: {self.description}"
+
     
     
     
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,56 +1,72 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import *
-
+from django.contrib import messages
 
 
 def index(request):
     if 'username' not in request.session:
         return redirect('./login')
     context = {
-        'username' : request.session['username'],
-        'blogs' : Blog.objects.all()
+        'username': request.session['username'],
+        'blogs': Blog.objects.all()
     }
     return render(request, 'all_blogs.html', context)
+
 
 def login(request):
     return render(request, 'login.html')
 
 
-
 def process_login(request):
-    user = User.objects.filter(username = request.POST['username'])
+    user = User.objects.filter(username=request.POST['username'])
     if not user.exists():
-        user = User.objects.create(username = request.POST['username'])
+        user = User.objects.create(username=request.POST['username'])
     request.session['username'] = request.POST['username']
-    
+
     return redirect('./')
 
 
 def new(request):
     return render(request, 'new_blog.html')
 
+
 def process_new(request):
     if 'username' not in request.session:
         return redirect('./login')
-    user = User.objects.filter(username = request.session['username']).first()
-    Blog.objects.create(user=user,description=request.POST['description'])
+    
+    errors = Blog.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+
+        return redirect(f"./new")
+    else:
+        user = User.objects.filter(
+            username=request.session['username']).first()
+    Blog.objects.create(user=user, description=request.POST['description'])
     return redirect('./')
+
 
 def create(request):
     return HttpResponse("create placeholder")
 
+
 def blog(request, number):
     context = {
-        'blog' : Blog.objects.get(id=number)
+        'blog': Blog.objects.get(id=number)
     }
-    return render(request,'blog.html',context)
+    return render(request, 'blog.html', context)
+
 
 def process_new_entry(request, number):
-    blog_entry = BlogEntry.objects.create(text=request.POST['text'],blog_id=number)
+    blog_entry = BlogEntry.objects.create(
+        text=request.POST['text'], blog_id=number)
     return redirect(f"../{number}")
+
 
 def edit(request, number):
     return HttpResponse(f"placeholder to edit blog number {number}")
+
 
 def delete(request, number):
     return redirect('/blogs')
